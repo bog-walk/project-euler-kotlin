8d734d3dd1755b64f635d67e1a812e904a78802f
Bog Walk <bogwalk.brackish@gmail.com>
2021-07-06 12:01:43 -0400

    Refactor all methods to return Long instead of Int. This takes into accoutn possibility that a very large prime number may be provided as N, requiring that it be returned as the sole factor.

--- src/main/kotlin/P3-LargestPrimeFactor.kt
+++ src/main/kotlin/P3-LargestPrimeFactor.kt
@@ -2,39 +2,32 @@ import kotlin.math.sqrt
 
 /**
  * Problem 3: Largest Prime Factor
- * Goal: Find the largest prime factor of N, given 10 <= N <= 10^12.
+ * Goal: Find the largest prime factor of N, given 2 <= N <= 10^12.
  * Prime factorisation involves finding set of prime factors that
  * multiply to form N. e.g. 12 = 2 * 2 * 3. There will only ever be a
  * unique set of prime factors for a number: Fundamental Theorem of Arithmetic.
  * e.g. The largest prime factor of 13195 is 29, from {5, 7, 13, 29}.
  */
 
-fun Int.isComposite(): Boolean {
+fun Long.isComposite(): Boolean {
     for (i in 2 until this) {
-        if (this % i == 0) return true
+        if (this % i == 0L) return true
     }
     return false
 }
 
-fun Long.divide(factor: Int): Pair<Boolean, Long> {
+fun Long.divide(factor: Long): Pair<Boolean, Long> {
     return Pair(this % factor == 0L, this / factor)
 }
 
 class LargestPrimeFactor {
-    fun largestPrime(primes: List<Int>): Int {
-        // could use max, but largest should be last element as ordered
-        return if (primes.isEmpty()) {
-            0
-        } else {
-            primes.last()
-        }
-    }
+    fun largestPrime(primes: List<Long>): Long = primes.maxOrNull()!!
 
     // Problematic for higher level constraint values (time out)
-    fun getPrimeFactors(n: Long): List<Int> {
-        var i = 1
+    fun getPrimeFactors(n: Long): List<Long> {
+        var i = 1L
         var dividend = n
-        val factors = mutableListOf<Int>()
+        val factors = mutableListOf<Long>()
         while (i <= n) {
             i++
             if (i.isComposite()) continue
@@ -50,16 +43,15 @@ class LargestPrimeFactor {
 
     fun getPrimeFactorsRecursive(
         n: Long,
-        primes: MutableList<Int> = mutableListOf()
-    ): List<Int> {
-        if (n < 2) return emptyList()
+        primes: MutableList<Long> = mutableListOf()
+    ): List<Long> {
         for (i in 2 until n) {
             if (n % i == 0L) {
-                primes.add(i.toInt())
+                primes.add(i)
                 return getPrimeFactorsRecursive(n / i, primes)
             }
         }
-        primes.add(n.toInt())
+        primes.add(n)
         return primes
     }
 
@@ -67,27 +59,27 @@ class LargestPrimeFactor {
      * 2 is the only even prime number so it is treated separately, so
      * as to allow stepping through iteration to odd numbers only later
      * on. No need to iterate until the target number as will not have
-     * a prime factor greater than its square root. 
+     * a prime factor greater than its square root.
      */
     fun getPrimeFactorsRecursiveImproved(
         n: Long,
-        primes: MutableList<Int> = mutableListOf()
-    ): List<Int> {
-        if (n < 2) return emptyList()
+        primes: MutableList<Long> = mutableListOf()
+    ): List<Long> {
         var num = n
         while (num % 2 == 0L) {
             primes.add(2)
             num /= 2
-            if (num < 2) return primes
         }
-        val maxFactor = sqrt(num.toDouble()).toInt()
-        for (i in 3..maxFactor step 2) {
-            if (num % i == 0L) {
-                primes.add(i)
-                return getPrimeFactorsRecursive(num / i, primes)
+        if (num > 1) {
+            val maxFactor = sqrt(num.toDouble()).toInt()
+            for (i in 3..maxFactor step 2) {
+                if (num % i == 0L) {
+                    primes.add(i.toLong())
+                    return getPrimeFactorsRecursiveImproved(num / i, primes)
+                }
             }
         }
-        primes.add(num.toInt())
+        if (num > 2) primes.add(num)
         return primes
     }
 }
--- src/test/kotlin/LargestPrimeFactorTest.kt
+++ src/test/kotlin/LargestPrimeFactorTest.kt
@@ -7,29 +7,31 @@ internal class LargestPrimeFactorTest {
     @ParameterizedTest(name="N={0} gives {1}")
     @CsvSource(
         // lower constraint values
-        "0, 0", "2, 2", "10, 5",
+        "2, 2", "3, 3", "10, 5",
         // normal values
         "17, 17", "48, 3", "147, 7", "330, 11",
         // larger values
         "13195, 29", "200000, 5", "600851475143, 6857",
+        // large prime numbers, including Euler's
+        "7919, 7919", "2147483647, 2147483647", "67280421310721, 67280421310721",
         // higher constraint values
-        "10000000000000000, 5"
+        "9007199254740991, 20394401", "10000000000000000, 5"
     )
-    fun testAlgorithm_largestPrime_recursive(n: Long, expected: Int) {
+    fun testAlgorithm_largestPrime_recursive(n: Long, expected: Long) {
         val tool = LargestPrimeFactor()
-        val primes = tool.getPrimeFactorsRecursive(n)
+        //val primes = tool.getPrimeFactorsRecursive(n)
         val primesImproved = tool.getPrimeFactorsRecursiveImproved(n)
-        assertEquals(expected, tool.largestPrime(primes))
+        //assertEquals(expected, tool.largestPrime(primes))
         assertEquals(expected, tool.largestPrime(primesImproved))
     }
 
     @Test
     fun testIsComposite() {
-        assertTrue(6.isComposite())
-        assertTrue(40.isComposite())
-        assertFalse(1.isComposite())
-        assertFalse(2.isComposite())
-        assertFalse(31.isComposite())
+        assertTrue(6L.isComposite())
+        assertTrue(40L.isComposite())
+        assertFalse(1L.isComposite())
+        assertFalse(2L.isComposite())
+        assertFalse(31L.isComposite())
     }
 
     @ParameterizedTest(name="{0} divided by {1}")
@@ -37,7 +39,7 @@ internal class LargestPrimeFactorTest {
         "12, 2, true, 6", "147, 3, true, 49",
         "12, 11, false, 1", "147, 2, false, 73"
     )
-    fun testAlgorithm_divide(n: Long, factor: Int, whole: Boolean, ans: Long) {
+    fun testAlgorithm_divide(n: Long, factor: Long, whole: Boolean, ans: Long) {
         val (actualFirst, actualSecond) = n.divide(factor)
         assertEquals(whole, actualFirst)
         assertEquals(ans, actualSecond)
@@ -45,9 +47,9 @@ internal class LargestPrimeFactorTest {
 
     @Test
     fun testGetPrimeFactors() {
-        val nums = listOf<Long>(1, 2, 3, 12, 48, 147, 330, 13195)
-        val expected = listOf(
-            emptyList(), listOf(2), listOf(3), listOf(2, 2, 3), listOf(2, 2, 2, 2, 3),
+        val nums = listOf<Long>(2, 3, 12, 17, 48, 147, 330, 13195)
+        val expected = listOf<List<Long>>(
+            listOf(2), listOf(3), listOf(2, 2, 3), listOf(17), listOf(2, 2, 2, 2, 3),
             listOf(3, 7, 7), listOf(2, 3, 5, 11), listOf(5, 7, 13, 29)
         )
         val tool = LargestPrimeFactor()
@@ -58,9 +60,9 @@ internal class LargestPrimeFactorTest {
 
     @Test
     fun testGetPrimeFactors_recursive() {
-        val nums = listOf<Long>(1, 2, 3, 12, 48, 147, 330, 13195, 200000, 600851475143)
-        val expected = listOf(
-            emptyList(), listOf(2), listOf(3), listOf(2, 2, 3), listOf(2, 2, 2, 2, 3),
+        val nums = listOf<Long>(2, 3, 12, 17, 48, 147, 330, 13195, 200000, 600851475143)
+        val expected = listOf<List<Long>>(
+            listOf(2), listOf(3), listOf(2, 2, 3), listOf(17), listOf(2, 2, 2, 2, 3),
             listOf(3, 7, 7), listOf(2, 3, 5, 11), listOf(5, 7, 13, 29),
             listOf(2, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5),
             listOf(71, 839, 1471, 6857)
