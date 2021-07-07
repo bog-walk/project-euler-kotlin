8cfbae50513a7d811ba6cd99b5dd245f4f1e2845
Bog Walk <bogwalk.brackish@gmail.com>
2021-07-06 22:29:26 -0400

    Write unit tests for SmallestMultiples class.
    Issues:
    - Current solution to Problem 5 takes too long for upper constraints,
    starting at N >= 30.

--- src/main/kotlin/P5-SmallestMultiple.kt
+++ src/main/kotlin/P5-SmallestMultiple.kt
@@ -4,26 +4,28 @@
  * in the range 1..N, with 1 <= N <= 40.
  * e.g. The smallest number that can be evenly divided by every number from 1 to 10
  * is 2520.
- * Tests: 1..3 = 6; 1..10 = 2520
  */
 
 class SmallestMultiple {
-    fun smallestMultiple(rangeMax: Int): Int {
+    fun smallestMultiple(rangeMax: Int): Long {
         return when (rangeMax) {
-            1 -> 1
-            2 -> 2
+            1 -> 1L
+            2 -> 2L
+            3 -> 6L
+            4 -> 12L
             else -> {
-                val range = (rangeMax - 1) downTo (rangeMax / 2 + 1)
-                lcm(rangeMax, range)
+                val range = (rangeMax - 2) downTo (rangeMax / 2 + 1)
+                val maxMultiple = 1L * rangeMax * (rangeMax - 1)
+                lcm(maxMultiple, range)
             }
         }
     }
 
-    private fun lcm(max: Int, range: IntProgression): Int {
-        var multiple = max * 2
+    private fun lcm(max: Long, range: IntProgression): Long {
+        var multiple = max * range.first
         outer@ while (true) {
             for (i in range) {
-                if (multiple % i != 0) {
+                if (multiple % i != 0L) {
                     multiple += max
                     continue@outer
                 }
--- src/test/kotlin/SmallestMultipleTest.kt
+++ src/test/kotlin/SmallestMultipleTest.kt
@@ -1,3 +1,19 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
-internal class SmallestMultipleTest
+internal class SmallestMultipleTest {
+    @ParameterizedTest(name="1..{0} = {1}")
+    @CsvSource(
+       // lower constraints
+        "1, 1", "2, 2", "3, 6",
+        // normal values
+        "6, 120", "10, 2520", "20, 232792560",
+        // higher constraints - TOO LONG
+        //"30, 2329089562800", "40, "
+    )
+    fun testSmallestMultiple(n: Int, expected: Long) {
+        val tool = SmallestMultiple()
+        assertEquals(expected, tool.smallestMultiple(n))
+    }
+}
