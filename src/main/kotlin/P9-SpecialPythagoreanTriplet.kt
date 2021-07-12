7d5aef466036527ff290dc2888a4df3121434877
Bog Walk <bogwalk.brackish@gmail.com>
2021-07-12 17:05:36 -0400

    Set goals for Problem 9

---
+++ src/main/kotlin/P9-SpecialPythagoreanTriplet.kt
@@ -0,0 +1,13 @@
+/**
+ * Problem 9: Special Pythagorean Triplet
+ * Goal: Given N, with 1 <= N <= 3000, check if there exists any
+ * Pythagorean triplet for which a + b + c = N. Find the max product
+ * a*b*c or return -1 if no triplets possible.
+ * Note that a Pythagorean triplet is a set of 3 natural numbers
+ * such that a < b < c && a^2 + b^2 = c^2.
+ * e.g. 3^2 + 4^2 = 5^2.
+ * Test: 12 -> product = 60 (above 3 * 4 * 50. 4 -> product = -1.
+ */
+
+class SpecialPythagoreanTriplet {
+}
