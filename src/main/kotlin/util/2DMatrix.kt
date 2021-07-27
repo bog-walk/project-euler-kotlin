7c4bbec74b37a0738ea200c890dbf56abc70ea5e
Bog Walk <bogwalk.brackish@gmail.com>
2021-07-27 16:58:08 -0400

    Create Matrix class to remove utility code for handling grids from solution code.
    Write unit tests for utility class.

---
+++ src/main/kotlin/util/2DMatrix.kt
@@ -0,0 +1,79 @@
+package util
+
+class IntMatrix2D(
+    val rows: Int,
+    val cols: Int,
+    val op: (() -> Int)? = null
+) : Iterable<IntArray> {
+    private val matrix = Array(rows) { IntArray(cols) { op?.invoke() ?: 0 } }
+
+    override fun toString(): String {
+        val output = StringBuilder()
+        matrix.forEachIndexed { i, row ->
+            output.append(row.joinToString(" ", "[ ", " ]"))
+            if (i < rows - 1) output.append("\n")
+        }
+        return output.toString()
+    }
+
+    operator fun plus(n: Int) {
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                matrix[i][j] += n
+            }
+        }
+    }
+
+    fun product(row: IntArray): Int {
+        return row.reduce { acc, n -> acc * n }
+    }
+
+    fun getDiagonals(): List<IntArray> {
+        if (rows != cols) return emptyList()
+        val leading = IntArray(cols)
+        val counter = IntArray(cols)
+        for ((step, i) in (0 until rows).withIndex()) {
+            leading[i] = matrix[i][i]
+            counter[i] = matrix[i][cols - 1 - step]
+        }
+        return listOf(leading, counter)
+    }
+
+    // To iterate through rows, not individual elements
+    override fun iterator(): Iterator<IntArray> {
+        return IntMatrix2DIterator(rows, matrix)
+    }
+
+    fun transpose(): IntMatrix2D {
+        val transposed = IntMatrix2D(cols, rows)
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                transposed[j][i] = matrix[i][j]
+            }
+        }
+        return transposed
+    }
+
+    operator fun get(j: Int): IntArray {
+        return matrix[j]
+    }
+
+    operator fun set(i: Int, value: IntArray) {
+        matrix[i] = value
+    }
+}
+
+class IntMatrix2DIterator(
+    private val rows: Int,
+    private val matrix: Array<IntArray>
+) : Iterator<IntArray> {
+    private var initRow = 0
+
+    override fun hasNext(): Boolean {
+        return initRow <= rows - 1
+    }
+
+    override fun next(): IntArray {
+        return matrix[initRow++]
+    }
+}
---
+++ src/test/kotlin/util/IntMatrix2DTest.kt
@@ -0,0 +1,66 @@
+package util
+
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+internal class IntMatrix2DTest {
+    @Test
+    fun testInitialState_default() {
+        val matrix = IntMatrix2D(3, 3)
+        val expected = "[ 0 0 0 ]\n" +
+                "[ 0 0 0 ]\n" +
+                "[ 0 0 0 ]"
+        assertEquals(expected, matrix.toString())
+    }
+
+    @Test
+    fun testInitialState_customPrefill() {
+        val matrix = IntMatrix2D(3, 3) { 100 }
+        val expected = "[ 100 100 100 ]\n" +
+                "[ 100 100 100 ]\n" +
+                "[ 100 100 100 ]"
+        assertEquals(expected, matrix.toString())
+    }
+
+    @Test
+    fun testGet() {
+        val matrix = IntMatrix2D(3, 3).apply {
+            this[1] = intArrayOf(1, 1, 1)
+        }
+        val expected = "[ 0 0 0 ]\n" +
+                "[ 1 1 1 ]\n" +
+                "[ 0 0 0 ]"
+        assertEquals(expected, matrix.toString())
+    }
+
+    @Test
+    fun testPlus() {
+        val matrix = IntMatrix2D(3, 3).apply { this + 4 }
+        val expected = "[ 4 4 4 ]\n" +
+                "[ 4 4 4 ]\n" +
+                "[ 4 4 4 ]"
+        assertEquals(expected, matrix.toString())
+    }
+
+    @Test
+    fun testProduct() {
+        val matrix = IntMatrix2D(3, 3).apply { this + 2 }
+        for (row in matrix.iterator()) {
+            assertEquals(8, matrix.product(row))
+        }
+    }
+
+    @Test
+    fun testGetDiagonals() {
+        val leading = intArrayOf(1, 2, 3)
+        val counter = intArrayOf(1, 2, 0)
+        val matrix = IntMatrix2D(3, 3).apply {
+            this[0] = intArrayOf(1, 1, 1)
+            this[1] = intArrayOf(2, 2, 2)
+            this[2] = intArrayOf(0, 1, 3)
+        }
+        val actual = matrix.getDiagonals()
+        assertTrue(leading.contentEquals(actual[0]))
+        assertTrue(counter.contentEquals(actual[1]))
+    }
+}
