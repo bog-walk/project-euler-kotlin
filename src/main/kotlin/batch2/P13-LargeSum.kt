7e2f73cbbfe71d2f5841cba0eaa2e59862da64b5
Bog Walk <bogwalk.brackish@gmail.com>
2021-07-29 14:13:29 -0400

    Add large test resource file & update tests to account for larger files.
    Fix solution method to overcome bug found for larger values.

--- src/main/kotlin/batch2/P13-LargeSum.kt
+++ src/main/kotlin/batch2/P13-LargeSum.kt
@@ -1,15 +1,35 @@
 package batch2
 
+import util.RollingQueue
+
 /**
  * Problem 13: Large Sum
  * Goal: Find the first 10 digits of the sum of N 50-digit
  * numbers, with 1 <= N <= 10^3.
- * Test sum = 2728190129, from
  *
  */
 
 class LargeSum {
     fun addInReverse(digits: List<String>): String {
-        
+        val n = digits.size
+        if (n == 1) return digits.first()
+        val output = RollingQueue<Int>(10)
+        val finalIndex = digits.first().length - 1
+        var carryOver = 0
+        for (i in finalIndex downTo 0) {
+            val sum = List(n + 1) {
+                if (it == n) carryOver else digits[it][i].digitToInt()
+            }.sum()
+            output.add(sum % 10)
+            carryOver = sum / 10
+        }
+        if (carryOver > 0) {
+            do {
+                val mod = carryOver % 10
+                output.add(mod)
+                carryOver /= 10
+            } while (carryOver != 0)
+        }
+        return output.reversed().joinToString("")
     }
 }
--- src/test/kotlin/batch2/LargeSumTest.kt
+++ src/test/kotlin/batch2/LargeSumTest.kt
@@ -3,15 +3,47 @@ package batch2
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.TestInstance
 import java.io.File
+import kotlin.test.Test
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class LargeSumTest {
-    private val fiftyDigs = getTestDigits("LargeSum5N")
+    private val hundredDigs = getTestDigits("src/test/resources/LargeSum100N")
+    private val fiftyDigs = getTestDigits("src/test/resources/LargeSum5N")
     private val tenDigs = listOf(
         "6041184107", "5351558590", "1833324270"
     )
+    private val threeDigs = listOf("123", "456", "789", "812", "234")
+
+    @Test
+    fun testSmallDigs() {
+        val tool = LargeSum()
+        val expected = threeDigs.sumOf { it.toInt() }.toString()
+        assertEquals(expected, tool.addInReverse(threeDigs))
+    }
+
+    @Test
+    fun testMidDigs() {
+        val tool = LargeSum()
+        assertEquals("1322606696", tool.addInReverse(tenDigs))
+    }
+
+    @Test
+    fun testLargeDigs() {
+        val tool = LargeSum()
+        assertEquals("2728190129", tool.addInReverse(fiftyDigs))
+    }
+
+    @Test
+    fun testVeryLargeDigs() {
+        val tool = LargeSum()
+        assertEquals("5537376230", tool.addInReverse(hundredDigs))
+    }
 
     private fun getTestDigits(filename: String): List<String> {
-        return File(filename).readLines()
+        val digits = mutableListOf<String>()
+        File(filename).forEachLine {
+            digits.add(it)
+        }
+        return digits
     }
 }
