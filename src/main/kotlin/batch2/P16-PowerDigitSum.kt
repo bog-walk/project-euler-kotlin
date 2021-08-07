d70a37119e72357b6e390f2673c6aebd8861cbff
Bog Walk <bogwalk.brackish@gmail.com>
2021-08-06 20:42:21 -0400

    Complete solution for Problem 16 & write tests

--- src/main/kotlin/batch2/P16-PowerDigitSum.kt
+++ src/main/kotlin/batch2/P16-PowerDigitSum.kt
@@ -1,5 +1,7 @@
 package batch2
 
+import java.math.BigInteger
+
 /**
  * Problem 16: Power Digit Sum
  * Goal: Find the sum of the digits of the number 2^N,
@@ -10,6 +12,7 @@ package batch2
 
 class PowerDigitSum {
     fun expDigSum(n: Int): Int {
-        
+        val power = BigInteger.TWO.pow(n).toString()
+        return power.map(Char::digitToInt).sum()
     }
 }
--- src/test/kotlin/batch2/PowerDigitSumTest.kt
+++ src/test/kotlin/batch2/PowerDigitSumTest.kt
@@ -7,8 +7,11 @@ import org.junit.jupiter.params.provider.CsvSource
 internal class PowerDigitSumTest {
     @ParameterizedTest(name="2^{0} -> {1}")
     @CsvSource(
-        "1, 2", "2, 4", "3, 8", "4, 7", "7, 11",
-        "15, 26", "99, ", "1000, ", "10000, "
+        // lower constraint
+        "1, 2", "2, 4", "3, 8",
+        "4, 7", "7, 11", "9, 8", "15, 26", "99, 107",
+        // higher constraint
+        "1000, 1366", "10000, 13561"
     )
     fun testExpDigSum(n: Int, expected: Int) {
         val tool = PowerDigitSum()
