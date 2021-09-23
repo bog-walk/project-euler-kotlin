b4e2368474b0724d8b47fb3a7c48780fcf88c2f6
Bog Walk <bogwalk.brackish@gmail.com>
2021-09-23 16:37:40 -0400

    Fix bugs in solutions for Problem 25 & add alternatives.
    Update unit tests for Problem 25.

--- src/main/kotlin/batch3/Problem25.kt
+++ src/main/kotlin/batch3/Problem25.kt
@@ -1,6 +1,7 @@
 package batch3
 
 import java.math.BigInteger
+import kotlin.math.*
 
 /**
  * Problem 25: N-digit Fibonacci Number
@@ -17,7 +18,38 @@ import java.math.BigInteger
  */
 
 class NDigitFibonacciNumber {
-    fun nDigitFibTerm(n: Int): Int {
+    /**
+     * Returns the Nth Fibonacci sequence number.
+     * Based on the closed-form formula:
+     * Fn = (Phi^n - Psi^n) / sqrt(5),
+     * with Phi = (1 + sqrt(5)) / 2 ~= 1.61803...
+     * & Psi = -Phi^-1.
+     * Rounding, using the nearest integer function, reduces the formula to:
+     * Fn = [Phi^n / sqrt(5)], where n >= 0.
+     * Truncation, using the floor function, would result instead in:
+     * Fn = [(Phi^n / sqrt(5)) + 0.5], where n >= 0.
+     */
+    fun nthFibUsingGoldenRatio(n: Int): Int {
+        val phi = (1 + sqrt(5.0)) / 2
+        return round(phi.pow(n) / sqrt(5.0)).toInt()
+    }
+
+    /**
+     * Based on the above formula being inverted to calculate log10 of
+     * each Fibonacci number, thereby returning the term that has the
+     * required amount of digits, without need to iterate.
+     * N = 5000 executes in 1ms.
+     */
+    fun nDigitFibTermByDigitsGolden(n: Int): Int {
+        val phi = (1 + sqrt(5.0)) / 2
+        return ceil((n - 1 + log10(5.0) / 2) / log10(phi)).toInt()
+    }
+
+    /**
+     * N = 5000 executes in 8118ms, significantly slower compared to above
+     * solution, but significantly faster than below solution.
+     */
+    fun nDigitFibTermBrute(n: Int): Int {
         var term = 7
         var fN = BigInteger.valueOf(13)
         var fNMinus1 = BigInteger.valueOf(8)
@@ -31,24 +63,22 @@ class NDigitFibonacciNumber {
     }
 
     /**
-     * Returns list of first terms in sequence to have (index + 2) digits.
+     * Iterative solution uses Golden Ratio to calculate the Fibonacci
+     * sequence numbers, which slows execution even from N > 10
+     * (due to exponential need to calculate Phi^N).
      */
-    fun fibTermByDigits(maxDigits: Int): List<Int> {
-        val terms = mutableListOf(7)
+    fun nDigitFibTermUsingGoldenRatio(n: Int): Int {
         var term = 7
-        var fN = BigInteger.valueOf(13)
-        var fNMinus1 = BigInteger.valueOf(8)
-        var digits = 3
-        while (digits <= maxDigits) {
-            term++
-            val fNMinus2 = fNMinus1
-            fNMinus1 = fN
-            fN = fNMinus1 + fNMinus2
-            if (fN.toString().length == digits) {
-                terms.add(term)
-                digits++
+        var fN = 13
+        val step = 4
+        while (fN.toString().length < n) {
+            term += step
+            fN = nthFibUsingGoldenRatio(term)
+            while (fN.toString().length < n) {
+                term++
+                fN = nthFibUsingGoldenRatio(term)
             }
         }
-        return terms
+        return term
     }
 }
--- src/test/kotlin/batch3/Problem25Test.kt
+++ src/test/kotlin/batch3/Problem25Test.kt
@@ -18,12 +18,38 @@ class NDigitFibonacciNumberTest {
         "5000, 23922"
     )
     fun testNDigitFibTerm(digits: Int, expected: Int) {
-        assertEquals(expected, tool.nDigitFibTerm(digits))
+        assertEquals(expected, tool.nDigitFibTermBrute(digits))
+        assertEquals(expected, tool.nDigitFibTermByDigitsGolden(digits))
     }
 
     @Test
-    fun testFibTermByDigits() {
+    fun testNthFibUsingGoldenRatio() {
+        val expected = listOf(
+            0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
+        )
+        val actual = List(18) { n -> tool.nthFibUsingGoldenRatio(n) }
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testNDigitFibTermSlowGolden() {
         val expected = listOf(7, 12, 17, 21, 26, 31, 36, 40)
-        assertEquals(expected, tool.fibTermByDigits(9))
+        for (n in 2..9) {
+            assertEquals(expected[n - 2], tool.nDigitFibTermUsingGoldenRatio(n))
+        }
+    }
+
+    @Test
+    fun testGetFibTermSpeedComparison() {
+        val n = 5000
+        val bruteStart = System.currentTimeMillis()
+        val ansBrute = tool.nDigitFibTermBrute(n)
+        val bruteStop = System.currentTimeMillis()
+        val goldSingleStart = System.currentTimeMillis()
+        val ansGoldSingle = tool.nDigitFibTermByDigitsGolden(n)
+        val goldSingleStop = System.currentTimeMillis()
+        println("Brute listing took: ${bruteStop - bruteStart}\n" +
+                "Gold single took: ${goldSingleStop - goldSingleStart}")
+        assertEquals(ansBrute, ansGoldSingle)
     }
 }
