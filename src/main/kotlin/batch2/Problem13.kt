5c5143ae55aa851b9496776f9372fc36e74c0230
Bog Walk <bogwalk.brackish@gmail.com>
2021-10-04 15:38:07 -0400

    Edit Problem 13 documentaion & clean code & tests.

--- src/main/kotlin/batch2/Problem13.kt
+++ src/main/kotlin/batch2/Problem13.kt
@@ -4,15 +4,29 @@ import util.RollingQueue
 
 /**
  * Problem 13: Large Sum
- * Goal: Find the first 10 digits of the sum of N 50-digit
- * numbers, with 1 <= N <= 10^3.
  *
+ * https://projecteuler.net/problem=13
+ *
+ * Goal: Find the first 10 digits of the sum of N 50-digit numbers.
+ *
+ * Constraints: 1 <= N <= 1e3
+ *
+ * e.g.: N.B. This is a scaled-down example (first 2 digits of N 5-digit numbers)
+ *       N = 3
+ *       34827, 93726, 90165
+ *       sum = 218718
+ *       1st 2 digits = 21
  */
 
 class LargeSum {
+
+    /**
+     * Given that BigInteger is not capable of handling this sum problem,
+     * this solution mimics manual addition from RTL.
+     */
     fun addInReverse(digits: List<String>): String {
         val n = digits.size
-        if (n == 1) return digits.first()
+        if (n == 1) return digits.first().slice(0..9)
         val output = RollingQueue<Int>(10)
         val finalIndex = digits.first().length - 1
         var carryOver = 0
@@ -25,8 +39,7 @@ class LargeSum {
         }
         if (carryOver > 0) {
             do {
-                val mod = carryOver % 10
-                output.add(mod)
+                output.add(carryOver % 10)
                 carryOver /= 10
             } while (carryOver != 0)
         }
--- src/test/kotlin/batch2/Problem13Test.kt
+++ src/test/kotlin/batch2/Problem13Test.kt
@@ -7,35 +7,33 @@ import kotlin.test.Test
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class LargeSumTest {
-    private val hundredDigs = getTestDigits("src/test/resources/LargeSum100N")
-    private val fiftyDigs = getTestDigits("src/test/resources/LargeSum5N")
+    private val tool = LargeSum()
+
+    private val threeDigs = listOf("123", "456", "789", "812", "234")
     private val tenDigs = listOf(
         "6041184107", "5351558590", "1833324270"
     )
-    private val threeDigs = listOf("123", "456", "789", "812", "234")
+    private val hundredDigs = getTestDigits("src/test/resources/LargeSum100N")
+    private val fiftyDigs = getTestDigits("src/test/resources/LargeSum5N")
 
     @Test
     fun testSmallDigs() {
-        val tool = LargeSum()
         val expected = threeDigs.sumOf { it.toInt() }.toString()
         assertEquals(expected, tool.addInReverse(threeDigs))
     }
 
     @Test
     fun testMidDigs() {
-        val tool = LargeSum()
         assertEquals("1322606696", tool.addInReverse(tenDigs))
     }
 
     @Test
     fun testLargeDigs() {
-        val tool = LargeSum()
         assertEquals("2728190129", tool.addInReverse(fiftyDigs))
     }
 
     @Test
     fun testVeryLargeDigs() {
-        val tool = LargeSum()
         assertEquals("5537376230", tool.addInReverse(hundredDigs))
     }
 
