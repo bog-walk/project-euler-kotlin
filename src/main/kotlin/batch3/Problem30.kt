8fd182a602c047a76af8daa3c05f747f493d2bce
Bog Walk <bogwalk.brackish@gmail.com>
2021-11-25 11:22:52 -0500

    Add solution & unit tests for Problem 30

---
+++ src/main/kotlin/batch3/Problem30.kt
@@ -0,0 +1,39 @@
+package batch3
+
+import kotlin.math.pow
+
+/**
+ * Problem 30: Digit Fifth Powers
+ *
+ * https://projecteuler.net/problem=30
+ *
+ * Goal: Calculate the sum of all numbers that can be written
+ * as the sum of the Nth power of their digits.
+ *
+ * Constraints: 3 <= N <= 6
+ *
+ * e.g.: N = 4
+ *       1634 = 1^4 + 6^4 + 3^4 + 4^4
+ *       8208 = 8^4 + 2^4 + 0^4 + 8^4
+ *       9474 = 9^4 + 4^4 + 7^4 + 4^4
+ *       sum = 1634 + 8208 + 9474 = 19316
+ */
+
+class DigitFifthPowers {
+    fun digitNthPowers(n: Int): List<Int> {
+        val nums = mutableListOf<Int>()
+        val start = maxOf(100, (10.0.pow(n - 2)).toInt())
+        val end = minOf(999999, (9.0.pow(n) * n).toInt())
+        for (num in start until end) {
+            var digits = num
+            var sumOfPowers = 0
+            while (digits > 0) {
+                sumOfPowers += (1.0 * digits % 10).pow(n).toInt()
+                if (sumOfPowers > num) break
+                digits /= 10
+            }
+            if (sumOfPowers == num) nums.add(num)
+        }
+        return nums
+    }
+}
