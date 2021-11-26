13bfc12f6addfd2532848b19e6a874d626fc5f72
Bog Walk <bogwalk.brackish@gmail.com>
2021-11-26 12:14:23 -0500

    Add recursive solution to Problem 31.
    Update Problem 31 documentation.
    
    Add speed test for Problem 31.

--- src/main/kotlin/batch4/Problem31.kt
+++ src/main/kotlin/batch4/Problem31.kt
@@ -24,6 +24,47 @@ class CoinSums {
     private val modulus = BigInteger.valueOf(1_000_000_007)
     private val coins = listOf(1, 2, 5, 10, 20, 50, 100, 200)
 
+    private val recursiveMemo = Array(100001) { Array<BigInteger>(8) { BigInteger.ZERO } }
+
+    /**
+     * Recursive solution optimised with memoisation using top-down approach.
+     * Repeatedly subtract each coin value from the target & sum combos previously
+     * calculated for smaller targets.
+     *
+     * @param [coin] Index of coin value from [coins] above. Default is the
+     * largest coin available (2 pounds).
+     * This parameter allows flexibility in method purpose ->
+     * e.g. Could find combos for making 10p using 2p (& lower coins) = 6,
+     * instead of making 10p using all possible coins = 11 combos.
+     *
+     * SPEED: 15764ms for N = 1e5
+     */
+    private fun recursiveCombos(n: Int, coin: Int): BigInteger {
+        if (coin < 1) return BigInteger.ONE
+        if (recursiveMemo[n][coin] > BigInteger.ZERO) return recursiveMemo[n][coin]
+        var target = n
+        var combos = BigInteger.ZERO
+        while (target >= 0) {
+            combos += recursiveCombos(target, coin - 1)
+            target -= coins[coin]
+        }
+        recursiveMemo[n][coin] = combos
+        return combos
+    }
+
+    fun countCoinCombosRecursive(n: Int, coin: Int = 7): Int {
+        return recursiveCombos(n, coin).mod(modulus).intValueExact()
+    }
+
+    /**
+     * Bottom-up approach that determines a target's combo based on:
+     * - the previous combo calculated for the coin with a smaller target, &
+     * - the previous combo calculated for a coin of lesser value.
+     *
+     * SPEED (BEST): 56ms for N = 1e5
+     * Better performance due less expensive loops (vs recursive function calls)
+     * & use of less memory with better cache-access.
+     */
     fun countCoinCombos(n: Int): Int {
         val combosByCoin = Array<BigInteger>(n + 1) { BigInteger.ZERO }
         // Base case for when 0p is needed
@@ -31,6 +72,9 @@ class CoinSums {
         coins.forEach { coin ->
             // Keep shifting forward as cannot give a higher value coin than needed
             for (i in coin..n) {
+                // Repeatedly adds previously counted ways to get remainder in coins
+                // when the coin value is already excluded.
+                // Target = 5p, using 2p coins -> 5-2 = index[3]
                 combosByCoin[i] += combosByCoin[i - coin]
             }
         }
