7bf5e8163167f32e836306a62ab7da97800ef6ba
Bog Walk <bogwalk.brackish@gmail.com>
2022-01-02 09:54:05 -0400

    Add alternate solution to Problem 43 & update speed test.

--- src/main/kotlin/batch5/Problem43.kt
+++ src/main/kotlin/batch5/Problem43.kt
@@ -28,15 +28,37 @@ import util.getPermutations
 class SubstringDivisibility {
 
     /**
-     * SPEED: 4.4709s for N = 9
+     * SPEED (WORST): 2.7708s for N = 9
      */
     fun sumOfPandigitalSubstrings(n: Int): Long {
         val primes = listOf(2, 3, 5, 7, 11, 13, 17)
         var sum = 0L
         val digits = ('0'..('0' + n)).toMutableList()
-        next@for (pandigital in getPermutations(digits, n + 1)){
+        next@for (pandigital in getPermutations(digits, n + 1)) {
             val subs = pandigital.windowed(3).drop(1)
             for ((i, sub) in subs.withIndex()) {
+                if (sub.toInt() % primes[i] != 0) continue@next
+            }
+            sum += pandigital.toLong()
+        }
+        return sum
+    }
+
+    /**
+     * Solution above is optimised by replacing built-in function
+     * that creates a new list of all windowed sub-lists, with a loop
+     * that slides over the permutation, checking each new windowed sub-
+     * list & breaking early if invalid.
+     *
+     * SPEED: 1.9302s for N = 9
+     */
+    fun sumOfPandigitalSubstrings_alt(n: Int): Long {
+        val primes = listOf(0, 2, 3, 5, 7, 11, 13, 17)
+        var sum = 0L
+        val digits = ('0'..('0' + n)).toMutableList()
+        next@for (pandigital in getPermutations(digits, n + 1)) {
+            for (i in 1 until n - 1) {
+                val sub = pandigital.substring(i..i+2)
                 if (sub.toInt() % primes[i] != 0) continue@next
             }
             sum += pandigital.toLong()
@@ -48,18 +70,18 @@ class SubstringDivisibility {
      * Project Euler specific implementation that only required the sum
      * of all 0 to 9 pandigital numbers that have substring divisibility.
      * Filtering the generated permutations through a sequence allowed the
-     * performance speed to be improved compared to the above solution.
+     * performance speed to be improved compared to the above solutions.
      *
      * Filter predicates based on the following:
      * - d_2..d_4 must be divisible by 2 so d_4 must be an even number.
-     * - d_4..d_6 mus be divisible by 5 so d_6 must be '0' or '5',
+     * - d_4..d_6 must be divisible by 5 so d_6 must be '0' or '5',
      * which is narrowed down to '5' as d_6..d_8 must be divisible by 11
      * and '0' would not allow pandigital options.
      * - d_3..d_5 must be divisible by 3, so d_3 + d_4 + d_5 must be also so.
      * - If eligible numbers are narrowed down manually, it is proven that
      * the d_1 and d_2 are either '1' or '4' and d_10 is either '7' or '9'.
      *
-     * SPEED (BETTER): 2.977s for N = 9
+     * SPEED (BEST): 1.7498s for N = 9
      */
     fun sumOf9PandigitalSubstrings(): Long {
         val digits = ('0'..'9').toMutableList()
@@ -67,10 +89,10 @@ class SubstringDivisibility {
         return getPermutations(digits, 10)
             .asSequence()
             .filter { perm ->
-                perm.first() !in "14" || perm.last() !in "79" ||
-                        perm[3].digitToInt() % 2 != 0 ||
-                        perm[4] != '5' ||
-                        perm.slice(2..4).map { it.digitToInt() }.sum() % 3 != 0
+                perm.first() in "14" && perm.last() in "79" &&
+                        perm[3].digitToInt() % 2 == 0 &&
+                        perm[5] == '5' &&
+                        perm.slice(2..4).map { it.digitToInt() }.sum() % 3 == 0
             }
             .mapNotNull { perm ->
                 var mapping: Long? = perm.toLong()
@@ -84,4 +106,9 @@ class SubstringDivisibility {
             }
             .sum()
     }
+}
+
+fun main() {
+    val tool = SubstringDivisibility()
+    println(tool.sumOf9PandigitalSubstrings())
 }
--- src/test/kotlin/batch5/Problem43Test.kt
+++ src/test/kotlin/batch5/Problem43Test.kt
@@ -3,32 +3,39 @@ package batch5
 import kotlin.system.measureNanoTime
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class SubstringDivisibilityTest {
     private val tool = SubstringDivisibility()
 
     @Test
     fun testSumOfPandigitalSubs() {
-        val expected = listOf(
-            22212, 711104, 12444480, 189838560, 1099210170, 1113342912, 16695334890
+        val expected = listOf<Long>(
+            22212, 711104, 12444480, 189838560, 1099210170, 1113342912
         )
-        for (n in 3..9) {
+        for (n in 3..8) {
             assertEquals(expected[n - 3], tool.sumOfPandigitalSubstrings(n))
+            assertEquals(expected[n - 3], tool.sumOfPandigitalSubstrings_alt(n))
         }
     }
 
     @Test
     fun testSumOf9PandigitalSubs_speed() {
-        val ogAns: Long
-        val seqAns: Long
+        val n = 9
+        val expected = 16695334890
+        val solutions = LongArray(3)
         val ogTime = measureNanoTime {
-            ogAns = tool.sumOfPandigitalSubstrings(9)
+            solutions[0] = tool.sumOfPandigitalSubstrings(n)
+        }
+        val altTime = measureNanoTime {
+            solutions[1] = tool.sumOfPandigitalSubstrings_alt(n)
         }
         val seqTime = measureNanoTime {
-            seqAns = tool.sumOf9PandigitalSubstrings()
+            solutions[2] = tool.sumOf9PandigitalSubstrings()
         }
         println("Original solution took: ${1.0 * ogTime / 1_000_000_000}s\n" +
+                "Alt solution took: ${1.0 * altTime / 1_000_000_000}s\n" +
                 "Sequence solution took: ${1.0 * seqTime / 1_000_000_000}s")
-        assertEquals(ogAns, seqAns)
+        assertTrue { solutions.all { expected == it } }
     }
 }
