91ee6a521b2c06902874df9128330f98d50967b4
Bog Walk <bogwalk.brackish@gmail.com>
2022-02-28 15:06:16 -0500

    Add solutions & unit tests for Problem 62.

---
+++ src/main/kotlin/batch6/Problem62.kt
@@ -0,0 +1,69 @@
+package batch6
+
+import util.combinatorics.permutationID
+
+/**
+ * Problem 62: Cubic Permutations
+ *
+ * https://projecteuler.net/problem=62
+ *
+ * Goal: Given N, find the smallest cubes for which exactly K permutations of its digits are the
+ * cube of some number < N.
+ *
+ * Constraints: 1e3 <= N <= 1e6, 3 <= K <= 49
+ *
+ * e.g.: N = 1000, K = 3
+ *       smallest cube = 41_063_625 (345^3)
+ *       permutations -> 56_623_104 (384^3), 66_430_125 (405^3)
+ */
+
+class CubicPermutations {
+    /**
+     * Solution stores all cubes in a dictionary with their permutation id as the key, thereby
+     * creating value lists of cubic permutations. The dictionary is then filtered for lists of K
+     * size.
+     *
+     * @return list of all K-sized lists of permutations that are cubes. These will already be
+     * sorted by the first (smallest) element of each list.
+     */
+    fun cubicPermutations(n: Int, k: Int): List<List<Long>> {
+        val cubePerms = mutableMapOf<String, List<Long>>()
+        for (num in 345 until n) {
+            val cube = 1L * num * num * num
+            val cubeID = permutationID(cube).joinToString("")
+            cubePerms[cubeID] = cubePerms.getOrDefault(cubeID, emptyList()) + cube
+        }
+        return cubePerms.values.filter { perms -> perms.size == k }
+    }
+
+    /**
+     * Project Euler specific implementation that requests the smallest cube for which exactly 5
+     * permutations of its digits are cube.
+     *
+     * Since exactly 5 permutations are required, without setting a limit, once the first
+     * permutations are found, the loop has to continue until the generated cubes no longer have
+     * the same amount of digits as the smallest cube in the permutation list. This ensures the
+     * list is not caught early.
+     *
+     * N.B. The permutations occur between 5027^3 and 8384^3.
+     */
+    fun smallest5CubePerm(): List<Long> {
+        val cubePerms = mutableMapOf<String, List<Long>>()
+        var longestID = "0"
+        var num = 345L
+        var maxDigits = 100
+        var currentDigits = 0
+        while (currentDigits <= maxDigits) {
+            val cube = num * num * num
+            val cubeID = permutationID(cube).joinToString("")
+            cubePerms[cubeID] = cubePerms.getOrDefault(cubeID, emptyList()) + cube
+            if (longestID == "0" && cubePerms[cubeID]?.size == 5) {
+                longestID = cubeID
+                maxDigits = cubeID.length
+            }
+            num++
+            currentDigits = cubeID.length
+        }
+        return cubePerms[longestID]!!
+    }
+}
---
+++ src/test/kotlin/batch6/Problem62Test.kt
@@ -0,0 +1,49 @@
+package batch6
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertContentEquals
+
+internal class CubicPermutationsTest {
+    private val tool = CubicPermutations()
+
+    @Test
+    fun `HR problem correct for lower constraints`() {
+        val n = 1000
+        val k = 3
+        val expected = listOf(listOf<Long>(41_063_625, 56_623_104, 66_430_125))
+        assertContentEquals(expected, tool.cubicPermutations(n, k))
+    }
+
+    @Test
+    fun `HR problem correct for mid constraints`() {
+        val n = 100_000
+        val k = 10
+        val expectedSize = 3
+        val expectedSmallests = listOf(
+            109_867_826_442_375, 150_178_532_496_264, 238_604_875_149_824
+        )
+        val actual = tool.cubicPermutations(n, k)
+        assertEquals(expectedSize, actual.size)
+        assertContentEquals(expectedSmallests, actual.map { it.first() })
+    }
+
+    @Test
+    fun `HR problem correct for upper constraints`() {
+        val n = 1_000_000
+        val k = 49
+        val expectedSize = 1
+        val expectedSmallest = 101_740_954_697_838_253
+        val actual = tool.cubicPermutations(n, k)
+        assertEquals(expectedSize, actual.size)
+        assertEquals(expectedSmallest, actual[0][0])
+    }
+
+    @Test
+    fun `PE problem correct`() {
+        val expected = listOf(
+            127_035_954_683, 352_045_367_981, 373_559_126_408, 569_310_543_872, 589_323_567_104
+        )
+        assertContentEquals(expected, tool.smallest5CubePerm())
+    }
+}
