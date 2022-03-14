240c3c22d666aa554f891fc9abeb15dd4e1cef5b
Bog Walk <bogwalk.brackish@gmail.com>
2022-03-14 16:36:27 -0400

    Add solutions & unit tests for Problem 77.

--- src/test/kotlin/batch7/Problem76Test.kt
+++ src/test/kotlin/batch7/Problem76Test.kt
@@ -10,7 +10,7 @@ internal class CountingSummationsTest {
     @ParameterizedTest(name="N = {0}")
     @CsvSource(
         // lower constraints
-        "2, 1", "3, 2", "4, 4,", "5, 6", "6, 10", "7, 14", "10, 41",
+        "2, 1", "3, 2", "4, 4", "5, 6", "6, 10", "7, 14", "10, 41",
         // mid constraints
         "80, 15796475", "100, 190569291", "350, 528158869",
         //upper constraints
